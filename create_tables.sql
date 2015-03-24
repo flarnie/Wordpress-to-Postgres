b2ce75399765614271fd173961328d3b88df12d3
Flarnie Marchan <fnonemaker@gmail.com>
2015-03-24 06:58:58 -0700

    Correct the script which creates table

--- create_tables.sql
+++ create_tables.sql
@@ -1,9 +1,10 @@
 CREATE TABLE wp_posts
-  (wp_id number not null,
+  (wp_id bigint not null,
    post_date date not null,
    post_date_gmt date not null,
    post_content text,
    post_title character(150),
    post_excerpt character(150),
-   post_status character(20)
+   post_status character(20),
+   post_link character(150)
   );
