5da9ecca21a0958b377958a6878fba1028ce492c
Flarnie Marchan <fnonemaker@gmail.com>
2015-03-24 06:59:11 -0700

    Add nearly finished script for creating SQL insert statements
    TODO: escape single quotes in text entries.

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 var fs = require('fs');
 var parseString = require('xml2js').parseString;
+/**
+ * createSQLInsertStatement
+ * @param {Object} postObject
+ * @return {String}
+ */
+var createSQLInsertStatement = function(postObject) {
+  var postId = postObject['wp:post_id'][0];
+  var postDate = postObject['wp:post_date'][0].slice(0, 10);
+  var postDateGMT = postObject['wp:post_date_gmt'][0].slice(0, 10);
+  var postContent = postObject['content:encoded'][0];
+  var postTitle = postObject['title'][0];
+  var postExcerpt = postObject['excerpt:encoded'][0];
+  var postStatus = postObject['wp:status'][0];
+  var postLink = postObject['link'][0].match(/flarnie.com(.+)$/)[0];
+
+  return "insert into wp_posts (wp_id, post_date, post_date_gmt, post_content, post_title, post_excerpt, post_status, post_link)\n"
+  + "  values (\n" +
+  "    '" + postId + "',\n" +
+  "    to_date('" + postDate + "', 'YYYY-MM-DD'),\n" +
+  "    to_date('" + postDateGMT + "', 'YYYY-MM-DD'),\n" +
+  "    '" + postContent + "',\n" +
+  "    '" + postTitle + "'\n" +
+  "    '" + postExcerpt + "'\n" +
+  "    '" + postStatus + "'\n" +
+  "    '" + postLink + "'\n" +
+  '  );\n\n';
+};
 
 /**
  * buildSQLScript
  *   - Uses command line argument for name of WXR file
  *   - Reads WXR file
- *   TODO: - Converts post objects into values for db
- *   TODO: - Creates series of SQL 'insert' commands
- *   TODO: - Writes the series of commands into an output file.
+ *   - Converts post objects into values for db
+ *   - Creates series of SQL 'insert' commands
+ *   - Writes the series of commands into an output file.
  *
  *   Yes I know this is not the most space or time efficient approach.
  */
@@ -22,10 +49,13 @@ var buildSQLScript = function(){
     if (err) {
       throw new Error(('xml2js failed with error: ' + err));
     } else {
-      console.log(result);
+      var postObjects = result.rss.channel[0].item;
+      var sqlStatements = [];
+      postObjects.forEach(function(postObject) {
+        sqlStatements.push(createSQLInsertStatement(postObject));
+      });
+      fs.writeFileSync('insertWPPosts-' + Date.now() + '.sql', sqlStatements.join(''));
     }
-    //var sqlStatements =  generate the sqlStatements and push into array.
-    //fs.writeFileSync('insertWPPosts-' + Date.now() + '.sql', sqlStatements.join(''));
   });
 };
 
